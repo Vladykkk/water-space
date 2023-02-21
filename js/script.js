158c70b9ff267a730e03d6821363cd92a66ac5b2
rybakster <rubakvlad7@gmail.com>
2023-02-21 18:14:46 +0200

    do header menu looking like in layout

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-"use strict";
-
 // Menu burger
-
 document.addEventListener("click", documentActions);
 
 function documentActions(e) {
 	const targetElement = e.target;
 
+	// Open and close menu burger
 	if (targetElement.closest(".icon-menu")) {
 		document.documentElement.classList.toggle("menu-open");
+	} else if (targetElement.closest(".header__logo")) {
+		if (document.documentElement.classList.contains("menu-open")) {
+			document.documentElement.classList.remove("menu-open");
+		}
 	}
 
+	// Scroll to section
 	if (targetElement.closest("[data-goto]")) {
 		document.documentElement.classList.contains("menu-open")
 			? document.documentElement.classList.remove("menu-open")
