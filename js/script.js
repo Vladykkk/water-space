2321dfe3241cb686ce223e48adb561bbd6ab68e8
rybakster <rubakvlad7@gmail.com>
2023-02-22 15:36:04 +0200

    added a map, and button in index.html

--- js/script.js
+++ js/script.js
@@ -32,3 +32,38 @@ function documentActions(e) {
 		e.preventDefault();
 	}
 }
+
+// Initialize the map with the center point and zoom level
+let map = new google.maps.Map(document.getElementById("map"), {
+	center: { lat: 48.379433, lng: 31.16558 },
+	zoom: 6,
+});
+
+// Define an array of safe drinking water locations
+let safeWaterLocations = [
+	{ lat: 49.841952, lng: 24.031592, name: "Lviv" },
+	{ lat: 50.4501, lng: 30.5234, name: "Kyiv" },
+	{ lat: 49.5683, lng: 34.5854, name: "Poltava" },
+];
+
+// Add markers for each safe drinking water location
+for (var i = 0; i < safeWaterLocations.length; i++) {
+	let marker = new google.maps.Marker({
+		position: safeWaterLocations[i],
+		map: map,
+		title: safeWaterLocations[i].name,
+	});
+}
+
+// Add a listener for the click event
+map.addListener("click", function (event) {
+	// Prompt the user to enter a location name
+	let name = prompt("Enter a location name:");
+
+	// Create a marker at the clicked location with the entered name
+	let marker = new google.maps.Marker({
+		position: event.latLng,
+		map: map,
+		title: name,
+	});
+});
