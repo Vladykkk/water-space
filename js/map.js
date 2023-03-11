a2e7d262044195e2bf8c135f1eaeb0a8aae2edfb
rybakster <rubakvlad7@gmail.com>
2023-03-12 00:19:21 +0200

    make a better map, and fixed errors with images path

---
+++ js/map.js
@@ -0,0 +1,68 @@
+// Map
+function initMap() {
+  let map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 49.841952, lng: 24.031592 },
+    zoom: 8,
+  });
+
+  function addMarker(position, info, iconUrl, properties) {
+    let marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      icon: {
+        url: iconUrl,
+        scaledSize: new google.maps.Size(30, 30),
+        anchor: new google.maps.Point(25, 25),
+      },
+    });
+
+    let content = "<div><h2>" + info + "</h2>";
+
+    if (properties && properties.length > 0) {
+      content += "<table>";
+      for (let i = 0; i < properties.length; i++) {
+        content +=
+          "<tr><td>" +
+          properties[i].property +
+          "</td><td>" +
+          properties[i].value +
+          "</td></tr>";
+      }
+      content += "</table>";
+    }
+
+    content += "</div>";
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content,
+    });
+
+    marker.addListener("click", function () {
+      infoWindow.open(map, marker);
+    });
+
+    return marker;
+  }
+
+  fetch("../markers.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.markers.length; i++) {
+        let marker = addMarker(
+          data.markers[i].position,
+          data.markers[i].info,
+          data.markers[i].icon,
+          data.markers[i].properties
+        );
+      }
+    })
+    .catch((error) => console.error("Error fetching marker data: ", error));
+}
+
+// Google api
+let script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAtFsHvCk5mTA-4DSi_0dfqDaqxbsf2Y4k&callback=initMap";
+script.defer = true;
+script.async = true;
+document.head.appendChild(script);
--- js/script.js
+++ js/script.js
@@ -4,86 +4,33 @@
 document.addEventListener("click", documentActions);
 
 function documentActions(e) {
-	const targetElement = e.target;
+  const targetElement = e.target;
 
-	// Open and close menu burger
-	if (targetElement.closest(".icon-menu")) {
-		document.documentElement.classList.toggle("menu-open");
-	} else if (targetElement.closest(".header__logo")) {
-		if (document.documentElement.classList.contains("menu-open")) {
-			document.documentElement.classList.remove("menu-open");
-		}
-	}
+  // Open and close menu burger
+  if (targetElement.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  } else if (targetElement.closest(".header__logo")) {
+    if (document.documentElement.classList.contains("menu-open")) {
+      document.documentElement.classList.remove("menu-open");
+    }
+  }
 
-	// Scroll to section
-	if (targetElement.closest("[data-goto]")) {
-		document.documentElement.classList.contains("menu-open")
-			? document.documentElement.classList.remove("menu-open")
-			: null;
+  // Scroll to section
+  if (targetElement.closest("[data-goto]")) {
+    document.documentElement.classList.contains("menu-open")
+      ? document.documentElement.classList.remove("menu-open")
+      : null;
 
-		const goTo = targetElement.closest("[data-goto]").dataset.goto;
-		const goToElement = document.querySelector(goTo);
-		const headerHeight = document.querySelector(".header").offsetHeight;
+    const goTo = targetElement.closest("[data-goto]").dataset.goto;
+    const goToElement = document.querySelector(goTo);
+    const headerHeight = document.querySelector(".header").offsetHeight;
 
-		if (goToElement) {
-			window.scrollTo({
-				top: goToElement.offsetTop - (headerHeight + 15),
-				behavior: "smooth",
-			});
-		}
-		e.preventDefault();
-	}
+    if (goToElement) {
+      window.scrollTo({
+        top: goToElement.offsetTop - (headerHeight + 15),
+        behavior: "smooth",
+      });
+    }
+    e.preventDefault();
+  }
 }
-
-function addMarker(map, position, info, properties) {
-	let marker = new google.maps.Marker({
-		position: position,
-		map: map,
-	});
-
-	let content = "<div><h2>" + info + "</h2>";
-
-	if (properties && properties.length > 0) {
-		content += "<table>";
-		for (let i = 0; i < properties.length; i++) {
-			content +=
-				"<tr><td>" +
-				properties[i].property +
-				"</td><td>" +
-				properties[i].value +
-				"</td></tr>";
-		}
-		content += "</table>";
-	}
-
-	content += "</div>";
-
-	let infoWindow = new google.maps.InfoWindow({
-		content: content,
-	});
-
-	marker.addListener("click", function () {
-		infoWindow.open(map, marker);
-	});
-
-	return marker;
-}
-
-let map = new google.maps.Map(document.getElementById("map"), {
-	center: { lat: 49.841952, lng: 24.031592 },
-	zoom: 8,
-});
-
-fetch("../markers.json")
-	.then((response) => response.json())
-	.then((data) => {
-		for (let i = 0; i < data.markers.length; i++) {
-			let marker = addMarker(
-				map,
-				data.markers[i].position,
-				data.markers[i].info,
-				data.markers[i].properties
-			);
-		}
-	})
-	.catch((error) => console.error("Error fetching marker data: ", error));
