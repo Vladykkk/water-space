f34a9d7f6d3a1c07227c51ecef9374eb26f4a888
Vladyslav <rubakvlad7@gmail.com>
2023-03-11 22:37:22 +0200

    upgrade a map

--- js/script.js
+++ js/script.js
@@ -35,24 +35,55 @@ function documentActions(e) {
 	}
 }
 
-// Initialize the map with the center point and zoom level
+function addMarker(map, position, info, properties) {
+	let marker = new google.maps.Marker({
+		position: position,
+		map: map,
+	});
+
+	let content = "<div><h2>" + info + "</h2>";
+
+	if (properties && properties.length > 0) {
+		content += "<table>";
+		for (let i = 0; i < properties.length; i++) {
+			content +=
+				"<tr><td>" +
+				properties[i].property +
+				"</td><td>" +
+				properties[i].value +
+				"</td></tr>";
+		}
+		content += "</table>";
+	}
+
+	content += "</div>";
+
+	let infoWindow = new google.maps.InfoWindow({
+		content: content,
+	});
+
+	marker.addListener("click", function () {
+		infoWindow.open(map, marker);
+	});
+
+	return marker;
+}
+
 let map = new google.maps.Map(document.getElementById("map"), {
 	center: { lat: 49.841952, lng: 24.031592 },
 	zoom: 8,
 });
 
-// Define an array of safe drinking water locations
-let safeWaterLocations = [
-	{ lat: 49.841952, lng: 24.031592, name: "Lviv" },
-	{ lat: 50.4501, lng: 30.5234, name: "Kyiv" },
-	{ lat: 49.5683, lng: 34.5854, name: "Poltava" },
-];
-
-// Add markers for each safe drinking water location
-for (var i = 0; i < safeWaterLocations.length; i++) {
-	let marker = new google.maps.Marker({
-		position: safeWaterLocations[i],
-		map: map,
-		title: safeWaterLocations[i].name,
-	});
-}
+fetch("../markers.json")
+	.then((response) => response.json())
+	.then((data) => {
+		for (let i = 0; i < data.markers.length; i++) {
+			let marker = addMarker(
+				map,
+				data.markers[i].position,
+				data.markers[i].info,
+				data.markers[i].properties
+			);
+		}
+	})
+	.catch((error) => console.error("Error fetching marker data: ", error));
